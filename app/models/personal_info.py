4090cb9f1cb34c4abf20b46321e2a5668a869421
EuropeanOtterJun <junseong23@naver.com>
2026-01-26 17:19:30 +0900

    [Feat] #6 개인정보 타입 및 모델 경로 설정

--- app/models/personal_info.py
+++ app/models/personal_info.py
@@ -1,30 +1,28 @@
 """
-개인정보 타입 상수
+개인정보 타입 및 모델 경로 설정
 """
-from enum import Enum
+PII_CATEGORIES = ["p_nm", "p_rrn", "p_add", "p_ip", "p_ph", "p_acn", "p_pp", "p_em"]
 
-
-class PersonalInfoType(str, Enum):
-    """개인정보 8종류"""
-    NAME = "name"  # 이름
-    RRN = "rrn"  # 주민번호
-    ADDRESS = "address"  # 집주소
-    IP_ADDRESS = "ip_address"  # IP주소
-    PHONE = "phone"  # 전화번호
-    ACCOUNT = "account"  # 계좌번호
-    PASSPORT = "passport"  # 여권번호
-    EMAIL = "email"  # 이메일
-
-
-# 개인정보 종류별 가중치
-PERSONAL_INFO_WEIGHTS = {
-    PersonalInfoType.RRN: 10,
-    PersonalInfoType.PASSPORT: 9,
-    PersonalInfoType.ACCOUNT: 8,
-    PersonalInfoType.NAME: 5,
-    PersonalInfoType.ADDRESS: 5,
-    PersonalInfoType.PHONE: 4,
-    PersonalInfoType.EMAIL: 3,
-    PersonalInfoType.IP_ADDRESS: 2,
+PII_NAMES = {
+    "p_nm": "이름",
+    "p_rrn": "주민번호",
+    "p_add": "주소",
+    "p_ip": "IP주소",
+    "p_ph": "전화번호",
+    "p_acn": "계좌번호",
+    "p_pp": "여권번호",
+    "p_em": "이메일"
 }
 
+CONFIDENCE_THRESHOLDS = {
+    "p_nm": 0.70,    # 이름
+    "p_rrn": 0.95,   # 주민번호
+    "p_add": 0.70,   # 주소
+    "p_ip": 0.80,    # IP주소
+    "p_ph": 0.70,    # 전화번호
+    "p_acn": 0.90,   # 계좌번호
+    "p_pp": 0.75,    # 여권번호
+    "p_em": 0.70     # 이메일
+}
+
+DEFAULT_MODEL_PATH = "ParkJunSeong/PIILOT_NER_Model"
